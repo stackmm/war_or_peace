6664135f45853f7b3ff41c01e3c89ad5992f0e84
Steven Stackhouse <stackmm@gmail.com>
2023-03-22 13:36:32 -0600

    add tests for readable attributes and card ranks for deck.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -13,28 +13,36 @@ RSpec.describe Deck do
     expect(deck).to be_an_instance_of(Deck)
   end
 
-  it "can read the rank of cards" do
+  it "has readable attributes" do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
 
+    expect(deck.cards).to be_an(Array)
+    expect(deck.cards).to eq([card1, card2, card3])
+  end
+  # expect(deck.rank_of_card_at(0)).to eq(12)
+  # expect(deck.rank_of_card_at(2)).to eq(14)
+
+  it "can get rank of card at specific index in array" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq([card1, card2, card3])
     expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(1)).to eq(3)
     expect(deck.rank_of_card_at(2)).to eq(14)
   end
 
-  # it "can identify high ranking cards" do
-  #   card1 = Card.new(:diamond, 'Queen', 12)
-  #   card2 = Card.new(:spade, '3', 3)
-  #   card3 = Card.new(:heart, 'Ace', 14)
-  #   cards = [card1, card2, card3]
-  #   deck = Deck.new(cards)
 
-  #   expect()
-  
-  # end
 
-  require "pry"; binding.pry
+
+
+  # require "pry"; binding.pry
 
 end
