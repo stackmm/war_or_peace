0c57cbe1bbcaaefafee391c31d16355654611fe2
Steven Stackhouse <stackmm@gmail.com>
2023-03-22 20:49:28 -0600

    add updated type method to turnrb

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,7 +5,19 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
-    @type = :basic
+    @type = ""
+  end
+
+  def type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      @type = :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      if @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+        @type = :mutually_assured_destruction
+      else
+        @type = :war
+      end
+    end
   end
 
   def winner
@@ -25,6 +37,7 @@ class Turn
       "No Winner"
     end
   end
+  
 
   def pile_cards
     if @type == :basic
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -40,6 +40,7 @@ RSpec.describe Turn do
       player1 = Player.new("Megan", deck1)
       player2 = Player.new("Aurora", deck2)
       turn = Turn.new(player1, player2)
+      turn.type
 
       expect(turn.player1).to be_an_instance_of(Player)
       expect(turn.player2).to be_an_instance_of(Player)
@@ -64,6 +65,7 @@ RSpec.describe Turn do
       player1 = Player.new("Megan", deck1)
       player2 = Player.new("Aurora", deck2)
       turn = Turn.new(player1, player2)
+      turn.type
       winner = turn.winner
 
       expect(winner.name).to eq("Megan")
@@ -84,6 +86,7 @@ RSpec.describe Turn do
       player1 = Player.new("Megan", deck1)
       player2 = Player.new("Aurora", deck2)
       turn = Turn.new(player1, player2)
+      turn.type
       winner = turn.winner
       turn.pile_cards
 
@@ -106,6 +109,7 @@ RSpec.describe Turn do
       player1 = Player.new("Megan", deck1)
       player2 = Player.new("Aurora", deck2)
       turn = Turn.new(player1, player2)
+      turn.type
       winner = turn.winner
       turn.pile_cards
       turn.award_spoils(winner)
@@ -113,6 +117,32 @@ RSpec.describe Turn do
       expect(turn.player1.deck.cards).to eq([card2, card5, card8, card1, card3])
       expect(turn.player2.deck.cards).to eq([card4, card6, card7])
     end
+
+    # it "can award spoils to the winner after a :war turn" do
+    #   card1 = Card.new(:heart, 'Jack', 11)
+    #   card2 = Card.new(:heart, '10', 10)
+    #   card3 = Card.new(:heart, '9', 9)
+    #   card4 = Card.new(:diamond, 'Jack', 11)
+    #   card5 = Card.new(:heart, '8', 8)
+    #   card6 = Card.new(:diamond, 'Queen', 12)
+    #   card7 = Card.new(:heart, '3', 3)
+    #   card8 = Card.new(:diamond, '2', 2)
+
+    #   deck1 = Deck.new([card1, card2, card5, card8])
+    #   deck2 = Deck.new([card4, card3, card6, card7])
+    #   player1 = Player.new("Megan", deck1)
+    #   player2 = Player.new("Aurora", deck2)
+    #   turn = Turn.new(player1, player2)
+    #   turn.type
+    #   winner = turn.winner
+      
+    #   turn.pile_cards
+    
+    #   turn.award_spoils(winner)
+      
+    #   expect(turn.player1.deck.cards).to eq([card8])
+    #   expect(turn.player2.deck.cards).to eq([card7, card1, card2, card5, card4, card3, card6])
+    # end
   end
 
 end
