a92a5ad4bda68d92cdeffddcfe5b79ed5746df6a
Steven Stackhouse <stackmm@gmail.com>
2023-03-22 21:13:43 -0600

    add runner file

---
+++ lib/war_or_peace_runner.rb
@@ -0,0 +1,6 @@
+
+
+p "Welcome to War! (or Peace) This game will be played with 52 cards."
+p "The players today are Megan and Aurora."
+p "Type 'GO' to start the game!"
+p "------------------------------------------------------------------"
